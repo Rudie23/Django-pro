3c574e84019b7b86d1d6968f329dc6bd63ea949d
diego <ruandiego100@gmail.com>
2022-05-03 16:01:57 -0300

    Terminado

--- diegodev/tarefas/urls.py
+++ diegodev/tarefas/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from diegodev.tarefas.views import detalhe, home
+from diegodev.tarefas.views import detalhe, home, apagar
 
 app_name = 'tarefas'
 
 urlpatterns = [
     path('', home, name='home'),
     path('<int:tarefa_id>', detalhe, name='detalhe'),
+    path('apagar/<int:tarefa_id>', apagar, name='apagar'),
 ]
