41c7c6b713e7dcc5c03cf9e565cb1723ddb1c40a
diego <ruandiego100@gmail.com>
2022-05-02 22:20:02 -0300

    Consertado o problema do settings

--- diegodev/settings.py
+++ diegodev/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'diegodev.tarefas.views',
-    'diegodev.tarefas.models',
+    'diegodev.tarefas',
 ]
 
 MIDDLEWARE = [
