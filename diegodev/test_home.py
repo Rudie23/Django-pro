6edd377a529cbf98ac15801b8fa55f903ba8511d
Diego <ruandiego100@gmail.com>
2022-05-17 00:27:16 -0300

    CI melhorado

--- diegodev/test_home.py
+++ diegodev/test_home.py
@@ -1,6 +1,6 @@
 from django.test import Client
 
 
-def test_home_status_code(client:Client):
-    reposta = client.get('/')
-    assert reposta.status_code == 200
+def test_home_status_code(client: Client):
+    resposta = client.get('/')
+    assert resposta.status_code == 200
