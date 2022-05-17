54f7e363833b4a1a4a0d17e30662fc4832a0582d
Diego <ruandiego100@gmail.com>
2022-05-17 17:53:13 -0300

    Imersão 6

--- diegodev/tarefas/forms.py
+++ diegodev/tarefas/forms.py
@@ -7,3 +7,9 @@ class TarefaNovaForm(ModelForm):
     class Meta:
         model = Tarefa  # herdará de Tarefa o modelo de banco de dados
         fields = ['nome']  # Campos presentes no formulário, no caso apenas o nome
+
+
+class TarefaForm(ModelForm):
+    class Meta:
+        model = Tarefa  # herdará de Tarefa o modelo de banco de dados
+        fields = ['nome', 'feita']
