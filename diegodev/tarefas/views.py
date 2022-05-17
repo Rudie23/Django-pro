c9bb88ac300a48ab643b18df56f2f0f3cf739a8c
Diego <ruandiego100@gmail.com>
2022-05-17 12:34:01 -0300

    Imersão 3

--- diegodev/tarefas/views.py
+++ diegodev/tarefas/views.py
@@ -1,48 +1,6 @@
 
-from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
-
-from diegodev.tarefas.forms import TarefaNovaForm, TarefaForm
-from diegodev.tarefas.models import Tarefa
-
-# Create your views here.
 
 
 def home(request):
-    if request.method == 'POST':
-        form = TarefaNovaForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('tarefas:home'))
-        else:
-            tarefas_pendentes = Tarefa.objects.filter(feita=False).all()
-            tarefas_feitas = Tarefa.objects.filter(feita=True).all()
-            return render (
-                request, 'tarefas/home.html',
-                {'form': form,
-                'tarefas_pendentes': tarefas_pendentes,
-                'tarefas_feitas': tarefas_feitas
-                },
-                status=400)
-    tarefas_pendentes = Tarefa.objects.filter(feita=False).all()
-    tarefas_feitas = Tarefa.objects.filter(feita=True).all()
-    return render(request, 'tarefas/home.html',
-                  {'tarefas_pendentes': tarefas_pendentes,
-                   'tarefas_feitas': tarefas_feitas
-                   })
-
-
-def detalhe(request, tarefa_id):
-    if request.method == 'POST':
-        tarefa = Tarefa.objects.get(id=tarefa_id)
-        form = TarefaForm(request.POST, instance=tarefa)
-        if form.is_valid():
-            form.save()
-    return HttpResponseRedirect(reverse('tarefas:home'))
-
-
-def apagar(request, tarefa_id):
-    if request.method == 'POST':
-        Tarefa.objects.filter(id=tarefa_id).delete()
-    return HttpResponseRedirect(reverse('tarefas:home'))
+    return render(request, 'tarefas/home.html')
