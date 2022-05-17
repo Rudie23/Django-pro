f77851df6cc68f5050f84c35a808b4891643da79
Diego <ruandiego100@gmail.com>
2022-05-17 12:34:01 -0300

    Imersão 3

--- diegodev/tarefas/tests/test_tarefas_get.py
+++ diegodev/tarefas/tests/test_tarefas_get.py
@@ -1,11 +1,10 @@
-from django.urls import reverse
 import pytest
+from django.urls import reverse
 from pytest_django.asserts import assertContains
-from diegodev.tarefas.models import Tarefa
 
 
 @pytest.fixture
-def resposta(client, db):
+def resposta(client):
     resp = client.get(reverse('tarefas:home'))
     return resp
 
@@ -20,39 +19,3 @@ def test_formulario_presente(resposta):
 
 def test_botao_presente(resposta):
     assertContains(resposta, '<button type="submit"')
-
-
-@pytest.fixture
-def lista_de_tarefas_pendentes(db):
-    tarefas = [
-        Tarefa(nome='Tarefa 2', feita=False),
-        Tarefa(nome='Tarefa 1', feita=False),
-        ]
-    Tarefa.objects.bulk_create(tarefas)
-    return tarefas  
-
-
-@pytest.fixture
-def lista_de_tarefas_feitas(db):
-    tarefas = [
-        Tarefa(nome='Tarefa 3', feita=True),
-        Tarefa(nome='Tarefa 4', feita=True),
-        ]
-    Tarefa.objects.bulk_create(tarefas)
-    return tarefas
-
-    
-@pytest.fixture
-def resposta_com_lista_de_tarefas(client, lista_de_tarefas_pendentes, lista_de_tarefas_feitas):
-    resp = client.get(reverse('tarefas:home'))  
-    return resp    
-
-
-def test_lista_de_tarefas_pendentes_presentes(resposta_com_lista_de_tarefas, lista_de_tarefas_pendentes):
-    for tarefa in lista_de_tarefas_pendentes:
-        assertContains(resposta_com_lista_de_tarefas, tarefa.nome)
-
-
-def test_lista_de_tarefas_feitas_presentes(resposta_com_lista_de_tarefas, lista_de_tarefas_feitas):
-    for tarefa in lista_de_tarefas_feitas:
-        assertContains(resposta_com_lista_de_tarefas, tarefa.nome)
--- diegodev/tarefas/urls.py
+++ diegodev/tarefas/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from diegodev.tarefas.views import detalhe, home, apagar
+from diegodev.tarefas.views import home
 
 app_name = 'tarefas'
 
 urlpatterns = [
     path('', home, name='home'),
-    path('<int:tarefa_id>', detalhe, name='detalhe'),
-    path('apagar/<int:tarefa_id>', apagar, name='apagar'),
+
 ]
