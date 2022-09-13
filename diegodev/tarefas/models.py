1f83e61cf2b0a7f9b0c42c9dbb8af6df1260f875
Diego <ruandiego100@gmail.com>
2022-09-12 22:18:39 -0300

    Tudo ok

--- diegodev/tarefas/models.py
+++ diegodev/tarefas/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-# Modelo da tabela de banco de dados. Para criar as tabelas, basta ´da o comando python manage.py makemigrations
+# Modelo da tabela de banco de dados. Para criar as tabelas, basta dá o comando python manage.py makemigrations
 class Tarefa(models.Model):
     nome = models.CharField(max_length=128)
     feita = models.BooleanField(default=False)
