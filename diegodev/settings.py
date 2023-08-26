cf1f822178a3d30171062e2de22362b5a55ca46e
Diego <ruan.diego.anjos@outlook.com>
2023-08-25 22:35:17 -0300

    Updating the dependencies

--- diegodev/settings.py
+++ diegodev/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ovyxxe5z6n(a*+pv-i%mbki@c=b$e3+12$jh!p^1@k&9kcj%$b'
+SECRET_KEY = 'ovyxxe5z6n(a*+pv-i%mbki@c=b$e3+12$jh!p^1@k&9kcj%$b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
